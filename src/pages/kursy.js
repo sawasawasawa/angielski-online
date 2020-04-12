e59444524db13b51e036508033286afb774f0678
Ray <63162786+RaySawka@users.noreply.github.com>
2020-04-12 12:37:52 +0200

    Update kursy.js

--- src/pages/kursy.js
+++ src/pages/kursy.js
@@ -31,7 +31,7 @@ const Landing = (props) => (
                     <header className="major">
                         <h2>Wybierz kurs dla siebie</h2>
                     </header>
-                    <p>W przygotowaniu.</p>
+                    <p>Wkrótce wstawię tutaj więcej informacji. A teraz możesz napisać do mnie i powiedzieć mi czego potrzebujesz w nauce angielskiego. Ja Ci odpowiem, w jaki sposób mogę Ci pomóc.</p>
                 </div>
             </section>
             <section id="two" className="spotlights">
