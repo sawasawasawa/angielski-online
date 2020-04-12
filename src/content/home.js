c183b362cd37551a7546237ee77097e428566901
Ray <63162786+RaySawka@users.noreply.github.com>
2020-04-12 12:49:59 +0200

    Update home.js

--- src/content/home.js
+++ src/content/home.js
@@ -7,6 +7,7 @@ export default {
       <ul>
         <li>Nauka indywidualna, w parach lub małych grupach</li>
         <li>Język ogólny, w biznesie, dla pracowników nauki i konwersacje</li>
+        <li>Bez dojazdów, elastycznie, uczysz się, kiedy chcesz</li>
         {/*<li></li>*/}
       </ul>
     </>
