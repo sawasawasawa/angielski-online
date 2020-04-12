d57a4b76a5a65f8d56cd796e49d56b1665158d4b
Ray <63162786+RaySawka@users.noreply.github.com>
2020-04-12 12:26:09 +0200

    Update kursy.js

--- src/pages/kursy.js
+++ src/pages/kursy.js
@@ -31,7 +31,7 @@ const Landing = (props) => (
                     <header className="major">
                         <h2>Wybierz kurs dla siebie</h2>
                     </header>
-                    <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
+                    <p>W przygotowaniu.</p>
                 </div>
             </section>
             <section id="two" className="spotlights">
