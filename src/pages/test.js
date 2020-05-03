0cb97872dfe1fca25ddab7fb604829a0a916f407
Ray <63162786+RaySawka@users.noreply.github.com>
2020-05-03 22:17:54 +0200

    Update test.js

--- src/pages/test.js
+++ src/pages/test.js
@@ -35,7 +35,9 @@ const Elements = (props) => {
                 W każdym zadaniu poprawna jest tylko jedna odpowiedź. Postaraj się odpowiedzieć na jak najwięcej pytań.<br/><br/>
                 Podaj swój adres email - na ten adres wyślemy Ci wynik testu.<br/>
                 Jesteś gotowy? Kliknij przycisk 'Zacznij test' aby rozpocząć.<br/>
-                Powodzenia!
+                Powodzenia!<br/>
+                © 2020, Rajmund Sawka. Licensed under the Creative Commons Attribution Noncommercial 4.0 International License, CC BY NC. 
+                To view a copy of this license, visit: https://creativecommons.org/licenses/by-nc/4.0/
               </p>
 
               <form onSubmit={(e) => {
