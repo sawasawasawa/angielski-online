177bed16ae61c772a03cf4bf0dab51ef734697d0
Ray <63162786+RaySawka@users.noreply.github.com>
2020-05-11 10:16:53 +0200

    Update kursy.js

--- src/pages/kursy.js
+++ src/pages/kursy.js
@@ -43,7 +43,18 @@ const Landing = (props) => (
                             <header className="major">
                                 <h3>Angielski ogólny</h3>
                             </header>
-                            <p>Strona w przygotowaniu :).</p>
+                            <p>Miliony zwykłych ludzi zna obce języki. Jednak wielu innych myśli, że nie są w stanie się ich nauczyć. 
+                                Mówią więc:</br>
+                            <ul>
+                            <li>Nigdy nie miałem okazji nauczyć się obcego języka, a teraz jestem za stary.</li>
+                                Nie! Nigdy nie jest się za starym na naukę.
+                            <li>Nauczenie się wszystkich słów i gramatyki zabrałoby mi całe lata</li>
+                                Nie musisz nauczyć się całego języka - możesz zacząć się posługiwać językiem przy bardzo małej jego 
+                                znajomości, ucząc się go po trochu.
+                             <li>Gdy robię wiele błędów, czuję się zażenowany</li>
+                                Nie musisz. Robienie błędów jest częścią procesu nauki języka I inni to zrozumieją, więc nie przejmuj się.
+                                Po prostu używaj języka! 
+                                Z moją pomocą</p>
                             {/*<ul className="actions"> */}
                                {/* <li>*/}
                                 {/*    <Link to="/contact" className="button">Więcej</Link> */}
